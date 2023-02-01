0efb2f7bf011be40edb0d7cd4a0c26ecb8f0a451
NoelJasik <Noeloskar99@gmail.com>
2023-02-01 23:20:10 +0100

    Jumping

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,14 +5,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Base Stats")]
-    public float accelerationSpeed;
-    public float maxSpeed;
-    public float jumpForce;
-    public float friction;
+    [Header("Walking")]
+    [SerializeField] 
+    float accelerationSpeed;
+    [SerializeField] 
+    float maxSpeed;
+    [SerializeField] 
+    float friction;
+    
+    [Header("Jumping")]
+    [SerializeField] 
+    float jumpForce;
+    [SerializeField] 
+    float jumpStopForce;
+    // Kayoote time is the time the player can jump after leaving the ground
+    [SerializeField] 
+    float kayooteTime;
+    [SerializeField]
+    float jumpInputEnabledTime;
+    [SerializeField]
+    Transform groundChecker;
+    [SerializeField]
+    float groundCheckRadius;
+    [SerializeField]
+    LayerMask whatIsGround;
     [Header("References")]
     private Rigidbody rb;
     private GameObject cam;
+    [Header("Private Variables")] 
+    private bool isJumping;
+    private bool isButtonUp;
+    private bool canJump;
+    // isGrounded refers to the player "feet" being on the ground, while isTouchingGround is for the player being in contact with the ground
+    // This is necessary to prevent the player jumping when being near the ground but not on it, which is common when bunny hopping
+    // Raycast wouldn't work, because it would prevent jumping when standing on edges
+    private bool isGrounded;
+    private bool isTouchingGround;
+    private float jumpTimer;
+    private float kayooteTimer;
+
+
+
     [Header("Stuff to export")]
     public static Transform PlayerTransform;
 
@@ -35,12 +68,21 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         CameraMovement();
+        CanJump();
     }
     private void FixedUpdate()
     {
         Walking();
-        Friction();
+        if (isGrounded)
+        {
+            Friction();
+        }
+
+        Jump();
+
     }
+    
+    // Camera
 
     private void CameraMovement()
     {
@@ -55,6 +97,13 @@ public class PlayerMovement : MonoBehaviour
             cam.transform.localEulerAngles = new Vector3(300, 0, 0);
         }
     }
+    
+    private void OnDisable()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+    
+    // Walking
 
     private void Friction()
     {
@@ -85,8 +134,81 @@ public class PlayerMovement : MonoBehaviour
         }
     }
     
-    private void OnDisable()
+    // Jumping
+
+    private void CanJump()
     {
-        Cursor.lockState = CursorLockMode.None;
+        Collider[] hitColliders = new Collider[1];
+        isGrounded = Physics.OverlapSphereNonAlloc(groundChecker.position, groundCheckRadius, hitColliders,whatIsGround) == 1;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+            jumpTimer = jumpInputEnabledTime;
+        }
+        if (isJumping)
+        {
+            jumpTimer -= Time.deltaTime;
+        }
+        if (isJumping && jumpTimer <= 0)
+        {
+            isJumping = false;
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            isButtonUp = true;
+        }
+        if(isGrounded && isTouchingGround)
+        {
+            kayooteTimer = kayooteTime;
+        }
+        kayooteTimer -= Time.deltaTime;
+        canJump = kayooteTimer > 0;
     }
+    
+    private void Jump()
+    {
+        if (isJumping && !isButtonUp && canJump)
+        {
+            rb.AddForce(jumpForce * transform.up);
+            kayooteTimer = 0;
+            isJumping = false;
+        }
+
+        if (!isButtonUp) return;
+        if (rb.velocity.y >= 0)
+        {
+            Vector3 velocity = rb.velocity;
+            rb.velocity = new Vector3(velocity.x, velocity.y / jumpStopForce, velocity.z);
+        }
+        isButtonUp = false;
+        isJumping = false;
+    }
+    
+    // Ground Check
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundChecker.position, groundCheckRadius);
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.layer == 10 || other.gameObject.layer == 11)
+        {
+            isTouchingGround = true;
+        }
+    }
+    
+    private void OnCollisionExit(Collision other)
+    {
+        if(other.gameObject.layer == 10 || other.gameObject.layer == 11)
+        {
+            isTouchingGround = false;
+        }
+    }
+    
+    
+
 }
