a765d0475c7021e85473eb0daa8feb3a88caf567
Quanarie <qustaine@gmail.com>
2023-02-01 17:32:33 +0100

    Added soundManager and sounds

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; }
+
+    [SerializeField] private GameObject audioSourcePrefab;
+    
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        AudioSource newSource = Instantiate(audioSourcePrefab, transform).GetComponent<AudioSource>();
+        newSource.clip = clip;
+        newSource.Play();
+        Destroy(newSource.gameObject, clip.length);
+    }
+}
